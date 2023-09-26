2c00fec7fae600c13e4918d48b33e87cd347a9fe
Ludovick <ludovick.laine@gmail.com>
2023-09-26 16:30:23 +0200

    bob.py

--- bob.py
+++ bob.py
@@ -1,17 +1,20 @@
 import random
 import pygame
+from world import World
 
 class Bob:
-    def __init__(self, energy = 100, velocity = 1, mass = 1, perception = 0, memory_space = 0, max_energy = 200, position = (random.randrange(1,100), random.randrange(1,100)), world, en_fuite = False):
+    def __init__(self, energy = 100, velocity = 1, mass = 1, perception = 0, max_energy = 200, position = (x,y), world):
         self.energy = energy
         self.velocity = velocity
         self.mass = mass
         self.perception = perception
-        self.memory_space = memory_space
+        self.memory_space = []
         self.max_energy = max_energy
         self.position = position
-        self.en_fuite = en_fuite
+        self.en_fuite = False
         self.world = world
+
+        #(random.randrange(1,100), random.randrange(1,100) : pour la position
 
 
   
@@ -19,4 +22,4 @@ class Bob:
         return f"Bob {self.position} {self.velocity} {self.mass} {self.energy} {self.perception} {self.memory_space} {self.en_fuite} {self.world} {self.max_energy}"
     
 
-    def move(self)
+    def move(self): #fonction de déplacement du bob
