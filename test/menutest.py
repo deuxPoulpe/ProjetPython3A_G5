f9a6f1aa7ff33300536cfc23475aeb228999570e
Dorra Jerbi <dorrajerbi.univ@gmail.com>
2024-01-15 17:17:26 +0100

    menu corrigé Mehdi et Dorra

--- test/menutest.py
+++ test/menutest.py
@@ -1,185 +1,161 @@
 import pygame
 import sys
 
-# Initialisation de Pygame
-pygame.init()
+class Menu:
+    def __init__(self):
+        pygame.init()
 
-# Dimensions de la fenêtre
-LARGEUR, HAUTEUR = 800, 600
+        # Dimensions de la fenêtre
+        self.LARGEUR, self.HAUTEUR = 800, 600
 
-# Création de la fenêtre Pygame
-fenetre = pygame.display.set_mode((LARGEUR, HAUTEUR))
-pygame.display.set_caption("PROJET_PYTHON_2024_G5")
+        # Création de la fenêtre Pygame
+        self.fenetre = pygame.display.set_mode((self.LARGEUR, self.HAUTEUR))
+        pygame.display.set_caption("PROJET_PYTHON_2024_G5")
 
-# Constantes de couleur
-BLEU = (0, 0, 250)
-NOIR = (0, 0, 0)
-ROUGE = (250, 0, 0)
-VERT = (0, 250, 0)
-ROSE = (255, 192, 203)
+        # Constantes de couleur
+        self.NOIR = (0, 0, 0)
+        self.ROSE = (255, 192, 203)
 
-# Initialisation de la police
-policeChoisie = "GamepauseddemoRegular-RpmY6.otf"
-police = pygame.font.Font(policeChoisie, 36)
+       # Initialisation de la police
+        self.policeChoisie = "GamepauseddemoRegular-RpmY6.otf"
+        self.police = pygame.font.Font(self.policeChoisie, 36)
+        self.policeme = pygame.font.Font(None, 24)        
 
-# Fonction pour afficher du texte sur la fenêtre
-def afficher_texte(texte, x, y, taille=36):
-    fonte_texte = pygame.font.Font(policeChoisie, taille)
-    surface_texte = fonte_texte.render(texte, True, NOIR)
-    rect_texte = surface_texte.get_rect(center=(x, y))
-    fenetre.blit(surface_texte, rect_texte)
+        # Champs de texte
+        self.champs_texte = [pygame.Rect(350, 50 + i * 50, 300, 30) for i in range(10)]
 
-# Fonction du menu principal
-def menu_principal():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x_souris, y_souris = pygame.mouse.get_pos()
-                gestion_clic_menu(x_souris, y_souris)
+        # Couleurs des champs
+        self.couleur_active = pygame.Color('dodgerblue2')
+        self.couleur_inactive = pygame.Color('lightskyblue3')
+        self.couleur = self.couleur_inactive
+        self.textes_champs = [''] * 10
+        self.champ_actif = None
 
-        afficher_image_de_fond("fond.jpeg")
+        # Dictionnaire pour stocker les valeurs des variables
+        self.valeurs_variables = {}
 
-        afficher_texte("Game Of Life", LARGEUR // 2, 100, 56)
-        dessiner_bouton(ROSE, 300, 200, 200, 70, "Start")
-        dessiner_bouton(ROSE, 300, 300, 200, 70, "Options")
-        dessiner_bouton(ROSE, 300, 400, 200, 70, "Quitter")
+    def afficher_texte(self, texte, x, y, taille=36):
+        fonte_texte = pygame.font.Font(self.policeChoisie, taille)
+        surface_texte = fonte_texte.render(texte, True, self.NOIR)
+        rect_texte = surface_texte.get_rect(center=(x, y))
+        self.fenetre.blit(surface_texte, rect_texte)
 
-        pygame.display.update()
+    def afficher_image_de_fond(self, chemin_image):
+        arriere_plan = pygame.image.load(chemin_image)
+        arriere_plan = pygame.transform.scale(arriere_plan, (self.LARGEUR, self.HAUTEUR))
+        self.fenetre.blit(arriere_plan, (0, 0))
 
-# Classe pour une tuile
-class Tuile():
-    def __init__(self, x, y):
-        self.image = pygame.image.load("fond.jpeg")
-        self.rect = self.image.get_rect(topleft=(x, y))
+    def dessiner_bouton(self, couleur, x, y, largeur, hauteur, texte):
+        pygame.draw.ellipse(self.fenetre, couleur, (x, y, largeur, hauteur))
+        self.afficher_texte(texte, x + largeur // 2, y + hauteur // 2)
 
-# Fonction pour convertir des coordonnées isométriques
-def coordonnees_iso(x, y):
-    return x - y, (x + y) / 2
+    def afficher_formulaire(self, variables):
+        pygame.init()
+        self.afficher_image_de_fond("fond.jpeg")
 
-# Fonction pour mettre à jour la fenêtre
-def mettre_a_jour_fenetre():
-    afficher_image_de_fond("fond.jpeg")
+        for i, (variable, champ) in enumerate(zip(variables, self.champs_texte)):
+            # Affichage de la variable
+            surface_variable = self.police.render(variable, True, self.NOIR)
+            self.fenetre.blit(surface_variable, (50, 50 + i * 50))
 
-# Fonction pour afficher des tuiles
-def afficher_tuiles(valeur):
-    largeur_ecran, hauteur_ecran = fenetre.get_size()
-    for y in range(-valeur // 2, valeur // 2):
-        for x in range(-valeur // 2, valeur // 2):
-            nouvelle_tuile = Tuile(largeur_ecran // 4 + hauteur_ecran // 2 + x * TAILLE_TUILE,
-                                   hauteur_ecran // 2 - largeur_ecran // 4 + y * TAILLE_TUILE)
-            fenetre.blit(nouvelle_tuile.image, coordonnees_iso(nouvelle_tuile.rect.x, nouvelle_tuile.rect.y))
+            # Champ de saisie
+            pygame.draw.rect(self.fenetre, self.couleur, champ, 2)
+            surface_texte = self.policeme.render(self.textes_champs[i], True, self.NOIR)
+            largeur_texte = max(200, surface_texte.get_width() + 10)
+            champ.w = largeur_texte
+            self.fenetre.blit(surface_texte, (champ.x + 5, champ.y + 5))
 
-# Fonction pour l'interface de jeu
-def interface_jeu():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+            # Bouton de retour
+            self.dessiner_bouton(self.ROSE, 600, 505, 150, 50, "Retour")
 
-        fait = False
-        mettre_a_jour_fenetre()
-        afficher_tuiles(20)
-        pygame.display.update()
+        pygame.display.flip()
 
-# Champs de texte
-champs_texte = [pygame.Rect(200, 50 + i * 70, 300, 50) for i in range(8)]
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for i, champ in enumerate(self.champs_texte):
+                        if champ.collidepoint(event.pos):
+                            self.champ_actif = i
+                            self.couleur = self.couleur_active
+                        else:
+                            self.couleur = self.couleur_inactive
+                    if 600 <= event.pos[0] <= 700 and 500 <= event.pos[1] <= 530:
+                        return True
+                        # Attribuer les valeurs saisies aux variables
+                        for i, variable in enumerate(variables):
+                            self.valeurs_variables[variable] = int(self.textes_champs[i])
 
-# Couleurs des champs
-couleur_active = pygame.Color('dodgerblue2')
-couleur_inactive = pygame.Color('lightskyblue3')
-couleur = couleur_inactive
-textes_champs = [''] * 8
-champ_actif = policeChoisie
+                if event.type == pygame.KEYDOWN:
+                    if self.champ_actif is not None:
+                        if event.key == pygame.K_RETURN:
+                            try:
+                                value = int(self.textes_champs[self.champ_actif])  # Convertir en entier
+                                print("{}: {}".format(variables[self.champ_actif], value))
+                            except ValueError:
+                                print("Veuillez entrer une valeur numérique.")
 
+                            self.textes_champs[self.champ_actif] = ''  # Réinitialiser le champ de texte
+                        elif event.key == pygame.K_BACKSPACE:
+                            self.textes_champs[self.champ_actif] = self.textes_champs[self.champ_actif][:-1]
+                        else:
+                            self.textes_champs[self.champ_actif] += event.unicode
 
-# Fonction pour afficher un formulaire
-def afficher_formulaire(variables):
-    pygame.init()
-    global couleur
-    global champ_actif
+                if self.afficher_formulaire(variables):
+                    print("Retour au menu principal")
+                    self.menu_principal()
 
-    afficher_image_de_fond("fond.jpeg")
-
-    for i, (variable, champ) in enumerate(zip(variables, champs_texte)):
-        # Affichage de la variable
-        surface_variable = police.render(variable, True, NOIR)
-        fenetre.blit(surface_variable, (50, 50 + i * 70))
-
-        # Champ de saisie
-        pygame.draw.rect(fenetre, couleur, champ, 2)
-        surface_texte = police.render(textes_champs[i], True, NOIR)
-        largeur_texte = max(200, surface_texte.get_width() + 10)
-        champ.w = largeur_texte
-        fenetre.blit(surface_texte, (champ.x + 5, champ.y + 5))
-        # Bouton de retour
-        
-        pygame.draw.rect(fenetre, ROSE, (600, 500, 120, 50))
-        afficher_texte("Retour", 660, 525)
-
-    pygame.display.flip()
-
-    # Liste de variables
-    variables = ['G.S.Larg', 'G.S.long', 'Energy', 'Velocity', 'Min energy', 'The mass', 'score Bp', 'memory']
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                for i, champ in enumerate(champs_texte):
-                    if champ.collidepoint(event.pos):
-                        champ_actif = i
-                        couleur = couleur_active
-                    else:
-                        couleur = couleur_inactive
-            if event.type == pygame.KEYDOWN:
-                if champ_actif is not None:
-                    if event.key == pygame.K_RETURN:
-                        print("Variable {}: {}".format(champ_actif + 1, textes_champs[champ_actif]))
-                        textes_champs[champ_actif] = ''
-                    elif event.key == pygame.K_BACKSPACE:
-                        textes_champs[champ_actif] = textes_champs[champ_actif][:-1]
-                    else:
-                        textes_champs[champ_actif] += event.unicode
-
-            afficher_formulaire(variables)
-
-# Fonction pour gérer les clics dans le menu
-def gestion_clic_menu(x, y):
-    if 300 <= x <= 500:
-        if 200 <= y <= 250:
+    def gestion_clic_menu(self, x, y):
+        bouton_demarrer = pygame.Rect(300, 200, 200, 50)
+        bouton_options = pygame.Rect(300, 300, 200, 50)
+        bouton_quitter = pygame.Rect(300, 400, 200, 50)
+        boutton_Retour = pygame.Rect(600, 500, 100, 30)  # Ajuster la taille du bouton
+        if bouton_demarrer.collidepoint(x, y):
             print("Démarrer")
-            interface_jeu()
-        elif 300 <= y <= 350:
+            self.interface_jeu()
+        elif bouton_options.collidepoint(x, y):
             print("Options")
-            variables = ['Energy', 'Velocity', 'Min energy', 'The mass', 'score Bp', 'memory',]
-            afficher_formulaire(variables)
-        elif 400 <= y <= 450:
+            variables = ['number_of_river', 'number_of_lake', 'size_of_lake', 'max_height', 'nbFood', 'dayTick', 'Food_energy', 'generate_river', 'generate_lake', 'custo_terrain']
+            self.afficher_formulaire(variables)
+        elif bouton_quitter.collidepoint(x, y):
             pygame.quit()
             sys.exit()
 
-# Fonction pour dessiner un bouton sur la fenêtre
-def dessiner_bouton(couleur, x, y, largeur, hauteur, texte):
-    pygame.draw.ellipse(fenetre, couleur, (x, y, largeur, hauteur))
-    afficher_texte(texte, x + largeur // 2, y + hauteur // 2)
+    def menu_principal(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x_souris, y_souris = pygame.mouse.get_pos()
+                    self.gestion_clic_menu(x_souris, y_souris)
 
-# Fonction pour afficher une image de fond sur la fenêtre
-def afficher_image_de_fond(chemin_image):
-    arriere_plan = pygame.image.load(chemin_image)
-    arriere_plan = pygame.transform.scale(arriere_plan, (LARGEUR, HAUTEUR))
-    fenetre.blit(arriere_plan, (0, 0))
+            self.afficher_image_de_fond("fond.jpeg")
 
-# Variables globales
-TAILLE_TUILE = 16
-fenetre_active = False
-champ_texte_actif = policeChoisie
+            self.afficher_texte("Game Of Life", self.LARGEUR // 2, 100, 56)
+            self.dessiner_bouton(self.ROSE, 300, 200, 200, 50, "Start")
+            self.dessiner_bouton(self.ROSE, 300, 300, 200, 50, "Options")
+            self.dessiner_bouton(self.ROSE, 300, 400, 200, 50, "Quitter")
 
+            pygame.display.update()
 
-# Boucle principale
+    def interface_jeu(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.mettre_a_jour_fenetre()
+            # Mettez votre interface de jeu ici
+            pygame.display.update()
+
+    def mettre_a_jour_fenetre(self):
+        self.afficher_image_de_fond("fond.jpeg")
+
 if __name__ == "__main__":
-    menu_principal()
+    menu = Menu()
+    menu.menu_principal()
